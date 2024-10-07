1ffe4ee152639a98b073713124961bb69e9d5fdf
polinka <pgrisko5@gmail.com>
2024-10-07 15:26:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-day =int(input("введите день:"))
-month = int(input("введите месяц:"))
-if(month == 3 and day >=1)or (month ==4)or(month ==5)or (month==6 and day <=31):
+day =int(input("введите день:")) #вывод данной надписи на консоль
+month = int(input("введите месяц:")) # вывод даной надписи на консоль
+if(month == 3 and day >=1)or (month ==4)or(month ==5)or (month==6 and day <=31):#условие для определения определенного сезона
     season ="весна"
-elif (month ==6 and day >=1) or (month ==7) or (month ==8)or (month ==8 and day <=31):
+elif (month ==6 and day >=1) or (month ==7) or (month ==8)or (month ==8 and day <=31): # условие для определения определенного сезона
     season ="лето"
-elif (month ==9 and day>=1) or (month==10) or (month ==11 and day<=30):
+elif (month ==9 and day>=1) or (month==10) or (month ==11 and day<=30): # условие для определения опредленного сезона
     season ="зима"
-    print(f"дата{day}.{month} относится к сезону:{season}")
+    print(f"дата{day}.{month} относится к сезону:{season}") # вывод конкретного сезона на консоль

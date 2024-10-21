08c3ea206dcabd409f2c55117b2fe15c7c605d65
polinka <pgrisko5@gmail.com>
2024-10-21 15:23:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-day =int(input("введите день:")) #вывод данной надписи на консоль
-month = int(input("введите месяц:")) # вывод даной надписи на консоль
-if(month == 3 and day >=1)or (month ==4)or(month ==5)or (month==6 and day <=31):#условие для определения определенного сезона
-    season ="весна"
-elif (month ==6 and day >=1) or (month ==7) or (month ==8)or (month ==8 and day <=31): # условие для определения определенного сезона
-    season ="лето"
-elif (month ==9 and day>=1) or (month==10) or (month ==11 and day<=30): # условие для определения опредленного сезона
-    season ="зима"
-    print(f"дата{day}.{month} относится к сезону:{season}") # вывод конкретного сезона на консоль
+day = int(input("Введите день: ")) # ввод числа "дня" 
+month = int(input("Введите месяц: ")) # ввод числа "месяца" 
+if (day < 1 or month <1):
+    print("Неверная дата")
+else:
+  if (month == 3 and day >= 1 and day <=31) or (month == 4 and day <=30) or (month == 5 and day <=30) : # оператор  if-elif-else
+    season = "Весна" # сезон в соответстивии с условием 
+  elif (month == 6 and day >= 1 and day <=30) or (month == 7 and day <=31) or (month == 8 and day <= 31): # оператор if-elif-else
+    season = "Лето" # сезон в соответстивии с условием 
+  elif (month == 9 and day >= 1) or (month == 10 and day <=31) or (month == 11 and day <= 30): # оператор ветвления if-elif-else
+    season = "Осень" # сезон в соответстивии с условием 
+  elif (month == 12 and day >=1 and day <=31) or (month == 1 and day <=31) or (month == 2 and day <=28):  # оператор if-elif-else
+    season = "Зима" # сезон в соответстивии с условием 
+  else: # оператор ветвления if-elif-else
+    season = "Ошибка" # условие не является истинным
+    print("Неверная дата") # неверное время 
+print(f"Дата {day}.{month} относится к сезону: {season}") # вывод даты с обозначением сезона
